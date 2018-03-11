1145a8eb88a4ca227a6089d6d5e6e05804133694
KsGin <imqqyangfan@gmail.com>
2018-03-11 15:14:57 +0800

    test env

--- OpenGL-Breakout/start.cpp
+++ OpenGL-Breakout/start.cpp
@@ -1,6 +1,90 @@
 #include <iostream>
 
-int main(int argc, char* argv[])
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#define WIDTH 1920
+#define HEIGHT 1080
+
+void InitGlfw() {
+	/*初始化glfw*/
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); //设置最大版本
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); //设置最小版本
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); //设置core-profile
+	glfwWindowHint(GLFW_RESIZABLE, false); //设置不可改变大小
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+}
+
+
+
+void DisPlayColor() {
+	static int r = 0;
+	static int g = 128;
+	static int b = 255;
+	static int dr = 1;
+	static int dg = 1;
+	static int db = 1;
+
+	glClearColor(r / 255.0f, g / 255.0f, b / 255.0f, 1.0f);
+
+	r += dr;
+	g += dg;
+	b += db;
+
+	if (r > 255 || r < 0) dr = -dr;
+	if (g > 255 || g < 0) dg = -dg;
+	if (b > 255 || b < 0) db = -db;
+
+	glClear(GL_COLOR_BUFFER_BIT);
+
+}
+
+
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode) {
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, GL_TRUE);	}
+
+}
+
+
+
+int main()
 {
-	std::cout << "Game Start!" << std::endl;
+	InitGlfw();
+	//创建一个窗口
+	GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Empty Window", nullptr, nullptr);
+
+	//设置窗口环境
+	glfwMakeContextCurrent(window);
+
+	if (window == nullptr) {
+		std::cout << "Faild to create glfw window" << std::endl;
+		glfwTerminate();
+	}
+
+	//设置glew
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK) {
+		std::cout << "Faild to init glew" << std::endl;
+		return -1;
+	}
+
+	//设置位置
+	glViewport(0, 0, WIDTH, HEIGHT);
+	glfwSetKeyCallback(window, key_callback);
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+		DisPlayColor();
+		glfwSwapBuffers(window);
+
+	}
+
+	return 0;
+
 }
